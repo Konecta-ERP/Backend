6b4991ade51ea16929a0f1d63994621ae702a356
Omar Elshereef <omarelshereef@gmail.com>
2025-10-14 23:07:26 +0300

    fix load balanced error

--- Spring microservices/API-Gateway/src/main/java/Konecta/ERP/API_Gateway/routes/Routes.java
+++ Spring microservices/API-Gateway/src/main/java/Konecta/ERP/API_Gateway/routes/Routes.java
@@ -1,40 +1,37 @@
 package Konecta.ERP.API_Gateway.routes;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions;
-import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.servlet.function.HandlerFilterFunction;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.path;
 
 @Configuration
 public class Routes {
 
-    // Load-balanced RestClient for service discovery
     @Bean
     @LoadBalanced
     public RestClient.Builder loadBalancedRestClientBuilder() {
         return RestClient.builder();
     }
 
-    // Regular RestClient for direct URLs (like Eureka)
+    // This is the key - create a primary RestClient bean that's load-balanced
     @Bean
-    public RestClient.Builder restClientBuilder() {
-        return RestClient.builder();
+    @Primary
+    public RestClient restClient(@LoadBalanced RestClient.Builder builder) {
+        return builder.build();
     }
-
 
     @Bean
     public RouterFunction<ServerResponse> exampleServiceRoute() {
         return route("service-example")
-                .route(path("/api/example/**"), HandlerFunctions.http("lb://service-example"))
+                .route(path("/api/example/**"), http("lb://service-example"))
                 .build();
     }
-
 }
